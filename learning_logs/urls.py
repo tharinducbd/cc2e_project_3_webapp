102449419850a5fe3f67d88a792bda2dc4ae9a66
tharindu_dissanayake_88 <tharinduchanaka@gmail.com>
2023-04-01 11:06:37 +0530

    url for topics

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,7 +5,10 @@ from django.urls import path
 from . import views
 
 app_name = 'learning_logs'
+
 urlpatterns = [
-    # Home page
+    # Home page.
     path('', views.index, name='index'),
+    # Page that shows all topics.
+    path('topics/', views.topics, name='topics'),
 ]
